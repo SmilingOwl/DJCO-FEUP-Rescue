d2978c0daa084a8c39f8306d289babce80be9626
Juliana Marques <julianamcm05@gmail.com>
2020-02-29 21:16:59 +0000

    Add right and left movement

---
+++ FEUP Rescue/Assets/PlayerMovement2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public HeroController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
