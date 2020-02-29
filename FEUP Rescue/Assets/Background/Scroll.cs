19958b895fa2fbe399becf4f4f963b7df97144bd
helenaMontenegro <up201604184@fe.up.pt>
2020-02-29 18:31:43 +0000

    Added scrolling background and moving thief

---
+++ FEUP Rescue/Assets/Background/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float background_speed = 0.3f;
+    public Renderer background_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        background_renderer.material.mainTextureOffset += new Vector2(background_speed * Time.deltaTime, 0f);
+    }
+}
