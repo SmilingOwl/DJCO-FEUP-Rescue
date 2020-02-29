3990ebe454809471d37c529a7345ca9684f9ba4d
Juliana Marques <julianamcm05@gmail.com>
2020-02-29 21:31:19 +0000

    Add jump movement to Hero

--- FEUP Rescue/Assets/PlayerMovement2D.cs
+++ FEUP Rescue/Assets/PlayerMovement2D.cs
@@ -9,18 +9,23 @@ public class PlayerMovement2D : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
 
     // Start is called before the first frame update
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
