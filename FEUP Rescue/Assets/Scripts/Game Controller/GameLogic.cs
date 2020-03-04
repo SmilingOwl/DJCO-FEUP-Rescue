3b1e75400dcefd634319313fca88f5a61379c85f
helenaMontenegro <up201604184@fe.up.pt>
2020-03-04 12:43:11 +0000

    Add collision to pc

---
+++ FEUP Rescue/Assets/Scripts/Game Controller/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public static GameLogic instance;
+    public int lives = 3;
+    int points = 0;
+
+    void Awake() {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncrementPoints() {
+        points++;
+        Debug.Log("Points: " + points);
+    }
+}
