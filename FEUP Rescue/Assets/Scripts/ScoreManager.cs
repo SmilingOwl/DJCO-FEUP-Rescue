3f07928462e2dacb58d9e357d50a2d01ebfb7220
Juliana Marques <julianamcm05@gmail.com>
2020-03-05 17:00:09 +0000

    Add score

---
+++ FEUP Rescue/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager scoreManager;
+    public TextMeshProUGUI text;
+    int score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(scoreManager == null)
+        {
+            scoreManager = this;
+        }
+    }
+
+   public void UpdateScore(int pcs)
+    {
+        score = pcs;
+        text.text = "X" + score.ToString();
+    }
+
+}
