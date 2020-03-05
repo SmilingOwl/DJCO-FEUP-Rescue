3899b06bad4af3379820266cff49e4ead68aea1d
Juliana Marques <julianamcm05@gmail.com>
2020-03-05 11:16:50 +0000

    Add health bar!

--- FEUP Rescue/Assets/Scripts/Hero/PlayerHealth.cs
+++ FEUP Rescue/Assets/Scripts/Hero/PlayerHealth.cs
@@ -7,16 +7,19 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     int currentHealth;
     public Animator animator;
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
         animator.SetTrigger("Hurt");
 
         if (currentHealth <= 0)
