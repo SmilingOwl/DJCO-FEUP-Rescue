146d81b49efe26c4fe4d35b09e08d96c19a1a7b1
Juliana Marques <julianamcm05@gmail.com>
2020-03-08 15:35:06 +0000

    Add bomb

--- FEUP Rescue/Assets/Scripts/Game Controller/ObjectPool.cs
+++ FEUP Rescue/Assets/Scripts/Game Controller/ObjectPool.cs
@@ -11,18 +11,21 @@ public class ObjectPool : MonoBehaviour
     public List<GameObject> dustbinPool;
     public List<GameObject> applePool;
     public List<GameObject> coffeePool;
+    public List<GameObject> bombPool;
     public GameObject bench;
     public GameObject pc;
     public GameObject trap;
     public GameObject dustbin;
     public GameObject apple;
     public GameObject coffee;
+    public GameObject bomb;
     public int pcAmount = 6; 
     public int benchAmount = 2;
     public int trapAmount = 3;
     public int dustbinAmount = 3;
     public int appleAmount = 1;
     public int coffeeAmount = 1;
+    public int bombAmount = 1;
 
     void Awake() {
         instance = this;
@@ -32,7 +35,7 @@ public class ObjectPool : MonoBehaviour
     void Start()
     {
         benchPool = new List<GameObject>();
-        for(int i = 0; i < benchAmount || i < trapAmount || i < dustbinAmount || i < pcAmount || i < appleAmount || i < coffeeAmount; i++) {
+        for(int i = 0; i < benchAmount || i < trapAmount || i < dustbinAmount || i < pcAmount || i < appleAmount || i < coffeeAmount || i < bombAmount; i++) {
             if(i < benchAmount) {
                 GameObject obj = (GameObject) Instantiate(bench);
                 obj.SetActive(false); 
@@ -66,6 +69,12 @@ public class ObjectPool : MonoBehaviour
                 obj.SetActive(false);
                 coffeePool.Add(obj);
             }
+            if (i < bombAmount)
+            {
+                GameObject obj = (GameObject)Instantiate(bomb);
+                obj.SetActive(false);
+                bombPool.Add(obj);
+            }
         }
     }
 
@@ -83,6 +92,8 @@ public class ObjectPool : MonoBehaviour
             pool = applePool;
         } else if(obj == "coffee") {
             pool = coffeePool;
+        }else if(obj == "bomb") {
+            pool = bombPool;
         }
 
         for (int i = 0; i < pool.Count; i++) {
---
+++ FEUP Rescue/Assets/Scripts/Items/BombController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+        BombInstantiator.instance.RemoveBomb(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Hero")
+        {
+            gameObject.SetActive(false);
+            BombInstantiator.instance.RemoveBomb(gameObject);
+            // add to ui
+        }
+    }
+}
