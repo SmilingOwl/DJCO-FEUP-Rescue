e0ddc23c61e627f4b0e85d0323df6227a4af3b6b
Juliana Marques <julianamcm05@gmail.com>
2020-03-09 12:13:25 +0000

    Delete useless functions

--- FEUP Rescue/Assets/Scripts/Thief/Thief.cs
+++ FEUP Rescue/Assets/Scripts/Thief/Thief.cs
@@ -64,24 +64,7 @@ public class Thief : MonoBehaviour
         dead = true;
     }
 
-    public void LookAtPlayer()
-    {
-        Vector3 flipped = transform.localScale;
-        flipped.z *= -1f;
 
-        if (transform.position.x > player.position.x && isFlipped)
-        {
-            transform.localScale = flipped;
-            transform.Rotate(0f, 180f, 0f);
-            isFlipped = false;
-        }
-        else if (transform.position.x < player.position.x && !isFlipped)
-        {
-            transform.localScale = flipped;
-            transform.Rotate(0f, 180f, 0f);
-            isFlipped = true;
-        }
-    }
 
     public void Attack()
     {
