bb325a7d54ee8c8861f166083bac73c87c2c1a88
helenaMontenegro <up201604184@fe.up.pt>
2020-03-10 00:13:44 +0000

    Add bomb logic working

--- FEUP Rescue/Assets/Scripts/Items/BombExplode.cs
+++ FEUP Rescue/Assets/Scripts/Items/BombExplode.cs
@@ -7,6 +7,7 @@ public class BombExplode : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.gameObject.transform.localScale = new Vector3(1f, 1f, 0f);
+        animator.gameObject.transform.position += new Vector3(Thief.instance.transform.position.x - animator.gameObject.transform.position.x, 0f, 0f);
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -16,7 +17,7 @@ public class BombExplode : StateMachineBehaviour
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("Entered");
         animator.gameObject.SetActive(false);
+        Thief.instance.TakeDamage(100);
     }
 }
