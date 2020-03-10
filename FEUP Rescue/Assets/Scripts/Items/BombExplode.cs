43ea0be4e5832216053859e3db452c2cecbaee87
helenaMontenegro <up201604184@fe.up.pt>
2020-03-10 00:17:25 +0000

    Make explosion slightly bigger

--- FEUP Rescue/Assets/Scripts/Items/BombExplode.cs
+++ FEUP Rescue/Assets/Scripts/Items/BombExplode.cs
@@ -6,8 +6,10 @@ public class BombExplode : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.transform.localScale = new Vector3(1f, 1f, 0f);
-        animator.gameObject.transform.position += new Vector3(Thief.instance.transform.position.x - animator.gameObject.transform.position.x, 0f, 0f);
+        animator.gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 0f);
+        animator.gameObject.transform.position += new Vector3(Thief.instance.transform.position.x - animator.gameObject.transform.position.x, 
+            Thief.instance.transform.position.y + animator.gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2.0f -
+            animator.gameObject.transform.position.y, 0f);
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
