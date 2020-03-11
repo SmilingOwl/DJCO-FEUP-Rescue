8b742f35f53f3f24cc8d253d46a5bdda5cca416d
Juliana Marques <julianamcm05@gmail.com>
2020-03-11 20:56:10 +0000

    Fix repo part 2

---
+++ FEUP Rescue/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Enter");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Yay");
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
