bf4d3fd6397e28348d1d87602471611a90c93e18
Juliana Marques <julianamcm05@gmail.com>
2020-03-16 19:44:20 +0000

    Add song to apple

--- FEUP Rescue/Assets/Scripts/Items/AppleController.cs
+++ FEUP Rescue/Assets/Scripts/Items/AppleController.cs
@@ -3,29 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
+{  
     void OnBecameInvisible()
     {
         gameObject.SetActive(false);
         AppleInstantiator.instance.RemoveApple(gameObject);
     }
-
     
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Hero") {
+            FindObjectOfType<AudioManager>().Play("gainLife");
             gameObject.SetActive(false);
             AppleInstantiator.instance.RemoveApple(gameObject);
             collider.gameObject.GetComponent<PlayerHealth>().GetLife();
